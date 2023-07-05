5aaf7a66c654196084bab9dd4163df1b1eb261f6
SARON Ravuth <saronravuth955@gmail.com>
2023-07-05 23:56:40 +0700

    not done

--- TP13/API/schemas/index.js
+++
@@ -1,5 +0,0 @@
-const SignIn = require("./login");
-const SignUp = require("./register");
-const Category = require("./category");
-
-module.exports = { SignIn, SignUp, Category };
--- TP13/API/schemas/product.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-module.exports = Joi.object({
-  name: Joi.string().required(),
-  desc: Joi.string(),
-  imageUrl: Joi.string(),
-  price: Joi,
-});
